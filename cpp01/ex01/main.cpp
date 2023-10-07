0b881f6ac097330681f8f6a85be5b9395a32adce
Jaejun Shin <jaeshinkorean@gmail.com>
2023-10-07 18:23:51 +1030

    cpp1

---
+++ cpp01/ex01/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeshin <jaeshin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 16:29:57 by jaeshin           #+#    #+#             */
+/*   Updated: 2023/10/07 18:20:02 by jaeshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+int	main(void)
+{
+	int		n = 5;
+	Zombie*	zombies = zombieHorde(n, "jaejun");
+	for (int i = 0; i < n; i++)
+	{
+		zombies[i].announce();
+	}
+	delete[] zombies;
+	return (0);
+}
